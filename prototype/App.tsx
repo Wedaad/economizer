a5af6930e772b53d009f228f2c57a56503a452ab
Wedaad <wedaadh2@gmail.com>
2022-11-29 09:26:27 +0000

    add: Home screen

--- prototype/App.tsx
+++ prototype/App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import HomeScreen from './app/screens/HomeScreen';
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
+  console.log("App Running");
+
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* HOME SCREEN */}
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "eConomizer"}}
+          />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ prototype/app/screens/HomeScreen.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import {StyleSheet, View, Text, Button } from 'react-native';
+
+function HomeScreen({navigation}: any) {
+
+    const handleSPress = () => {
+        console.log("SignUp button was just pressed")
+        navigation.navigate("SignUp");
+        // console.log(firebase);
+    
+    };
+
+    const handleLPress = () => {
+        console.log("Login button was just pressed")
+        navigation.navigate("Login");
+    
+    };
+
+    return (
+            /* Holds main screen contents */
+            <View style={styles.screenLayout}>
+                <Text style={styles.title}>Welcome Screen</Text>
+                <View style={styles.btncontainer}>
+                    {/* Holds buttons */}
+                    <View style={styles.btns}>
+                        <Button title='Sign-Up' color="#BE7CFF" onPress={handleSPress} />
+                    </View>
+                    <View style={styles.btns}>
+                    <Button title='Login' color="#BE7CFF" onPress={handleLPress} />
+                    </View>
+                </View>
+            </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+    screenLayout: {
+        // borderWidth: 4,
+        // borderColor: 'orange',
+        flex: 1,
+        backgroundColor: 'white',
+        paddingTop: 30,
+        paddingHorizontal: 20,
+    },
+
+    btncontainer: {
+        // borderWidth: 4,
+        // borderColor: 'green',
+        marginTop: 280,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    title: {
+        // borderWidth: 4,
+        // borderColor: 'red',
+        padding: 8, 
+        fontSize: 24,
+        fontWeight: "bold",
+    },
+
+    btns: {
+        flex: 1,
+        margin: 10,
+    },
+});
+
+export default HomeScreen;
