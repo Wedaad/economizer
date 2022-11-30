22e26bfde38aae636b0a1a1f2af74075917331bd
Wedaad <wedaadh2@gmail.com>
2022-11-30 21:42:57 +0000

    feat: Link account screen

---
+++ prototype/app/screens/LinkAccountScreen.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {Text, View, StyleSheet, Button} from 'react-native';
+
+const LinkAccountScrceen = ({route}: any) => {
+
+    return (
+
+        <View style={styles.screenLayout}>
+            <View>
+                <Text style={styles.subTitle}>Hi <Text style={styles.subTitle}>{route.params.username}</Text></Text>
+                <Text style={styles.title}>Link eConomizer to your bank account</Text>
+            </View>
+
+            <Button title="Link Account Now"/>
+
+        </View>
+    );
+};
+  
+const styles = StyleSheet.create({
+    screenLayout: {
+        // borderWidth: 4,
+        // borderColor: 'orange',
+        padding: 20,
+        flex: 1,
+        backgroundColor: 'white'
+    },
+
+    title: {
+        // borderWidth: 4,
+        // borderColor: 'red',
+        margin: 10, 
+        fontSize: 22,
+        fontWeight: "bold",
+    },
+
+    subTitle: {
+
+        margin: 10, 
+        fontSize: 22,
+        textAlign: "center",
+
+    },
+
+  });
+  export default LinkAccountScrceen;
