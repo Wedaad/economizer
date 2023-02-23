bcfe85791098cc84f605eb17b007ab6cb7680df0
Wedaad <wedaadh2@gmail.com>
2023-02-23 16:28:19 +0000

    changed buttons on screen and added logo

--- prototype/app/screens/HomeScreen.tsx
+++ prototype/app/screens/HomeScreen.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
-import {StyleSheet, View, Text, Button } from 'react-native';
+import {StyleSheet, View, Text, Button, Image, Pressable  } from 'react-native';
+import { useFonts } from 'expo-font';
+import { isSearchBarAvailableForCurrentPlatform } from 'react-native-screens';
 
 function HomeScreen({navigation}: any) {
+
+    const [loaded] = useFonts({
+        Rubik: require('../assets/fonts/Rubik-Regular.ttf'),
+        // Rubik: require('../assets/fonts/Rubik-Bold.ttf'),
+    });
 
     const handleSPress = () => {
         console.log("SignUp button was just pressed")
@@ -22,23 +29,46 @@ function HomeScreen({navigation}: any) {
     
     };
 
+    const handleDPress = () => {
+        console.log("Dashboard button was just pressed")
+        navigation.navigate("Dashboard");
+    
+    };
+
+    const handleSOPress = () => {
+        console.log("Signout button was just pressed")
+        navigation.navigate("Logout");
+    
+    };
+
+    if(!loaded) {
+
+        return null;
+    }
+
+
     return (
             /* Holds main screen contents */
+            <>
             <View style={styles.screenLayout}>
-                <Text style={styles.title}>Welcome Screen</Text>
+                <Image source={require('../assets/economizer-logo5.jpg')} style={{width: 300, height: 300, resizeMode: 'contain', alignSelf: 'center', marginTop: 100}}/>
                 <View style={styles.btncontainer}>
                     {/* Holds buttons */}
                     <View style={styles.btns}>
-                        <Button title='Sign-Up' color="#BE7CFF" onPress={handleSPress} />
+
+                        <Pressable style={styles.login_button} onPress={handleLPress} >
+                            <Text style={styles.login_button_text}>Login</Text>
+                        </Pressable>
+
+                        <Pressable style={styles.signup_button} onPress={handleSPress}>
+                            <Text style={styles.signup_button_text}>Sign Up</Text>
+                        </Pressable>
                     </View>
-                    <View style={styles.btns}>
-                    <Button title='Login' color="#BE7CFF" onPress={handleLPress} />
-                    </View>
-                    <View style={styles.btns}>
-                        <Button title='Budgets' color="#BE7CFF" onPress={handleBPress} />
-                    </View>
+
                 </View>
             </View>
+            </>
+
     );
 }
 
@@ -56,8 +86,8 @@ const styles = StyleSheet.create({
     btncontainer: {
         // borderWidth: 4,
         // borderColor: 'green',
-        marginTop: 280,
-        flexDirection: 'row',
+        marginTop: 200,
+        flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center'
     },
@@ -65,14 +95,53 @@ const styles = StyleSheet.create({
     title: {
         // borderWidth: 4,
         // borderColor: 'red',
+        fontFamily: 'Rubik',
         padding: 8, 
         fontSize: 24,
-        fontWeight: "bold",
+        // fontWeight: "bold",
     },
 
     btns: {
-        flex: 1,
         margin: 10,
+    },
+
+    signup_button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 80,
+        borderRadius: 20,
+        elevation: 3,
+        backgroundColor: 'white',
+        borderWidth: 3,
+        borderColor: '#8B19FF',
+    },
+
+    login_button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 80,
+        borderRadius: 20,
+        elevation: 3,
+        backgroundColor: '#8B19FF',
+        marginBottom: 10,
+    },
+
+    signup_button_text: {
+        fontSize: 16,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: '#8B19FF',
+    },
+
+    login_button_text: {
+        fontSize: 16,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: 'white',
     },
 });
 
