52863c67a118ff4b88ba9bc38a4d3fa5c2c12b0a
Wedaad <wedaadh2@gmail.com>
2023-02-23 16:32:00 +0000

    fix: got rid of unused code

--- prototype/app/screens/HomeScreen.tsx
+++ prototype/app/screens/HomeScreen.tsx
@@ -1,43 +1,23 @@
 import React from 'react';
 import {StyleSheet, View, Text, Button, Image, Pressable  } from 'react-native';
 import { useFonts } from 'expo-font';
-import { isSearchBarAvailableForCurrentPlatform } from 'react-native-screens';
 
 function HomeScreen({navigation}: any) {
 
     const [loaded] = useFonts({
         Rubik: require('../assets/fonts/Rubik-Regular.ttf'),
-        // Rubik: require('../assets/fonts/Rubik-Bold.ttf'),
+
     });
 
     const handleSPress = () => {
         console.log("SignUp button was just pressed")
         navigation.navigate("SignUp");
-        // console.log(firebase);
     
     };
 
     const handleLPress = () => {
         console.log("Login button was just pressed")
         navigation.navigate("Login");
-    
-    };
-
-    const handleBPress = () => {
-        console.log("Budgets button was just pressed")
-        navigation.navigate("Budgets");
-    
-    };
-
-    const handleDPress = () => {
-        console.log("Dashboard button was just pressed")
-        navigation.navigate("Dashboard");
-    
-    };
-
-    const handleSOPress = () => {
-        console.log("Signout button was just pressed")
-        navigation.navigate("Logout");
     
     };
 
@@ -52,10 +32,9 @@ function HomeScreen({navigation}: any) {
             <>
             <View style={styles.screenLayout}>
                 <Image source={require('../assets/economizer-logo5.jpg')} style={{width: 300, height: 300, resizeMode: 'contain', alignSelf: 'center', marginTop: 100}}/>
+                {/* Holds buttons */}
                 <View style={styles.btncontainer}>
-                    {/* Holds buttons */}
-                    <View style={styles.btns}>
-
+                
                         <Pressable style={styles.login_button} onPress={handleLPress} >
                             <Text style={styles.login_button_text}>Login</Text>
                         </Pressable>
@@ -63,8 +42,6 @@ function HomeScreen({navigation}: any) {
                         <Pressable style={styles.signup_button} onPress={handleSPress}>
                             <Text style={styles.signup_button_text}>Sign Up</Text>
                         </Pressable>
-                    </View>
-
                 </View>
             </View>
             </>
@@ -92,29 +69,17 @@ const styles = StyleSheet.create({
         justifyContent: 'center'
     },
 
-    title: {
-        // borderWidth: 4,
-        // borderColor: 'red',
-        fontFamily: 'Rubik',
-        padding: 8, 
-        fontSize: 24,
-        // fontWeight: "bold",
-    },
-
-    btns: {
-        margin: 10,
-    },
-
     signup_button: {
         alignItems: 'center',
         justifyContent: 'center',
         paddingVertical: 12,
-        paddingHorizontal: 80,
-        borderRadius: 20,
+        paddingHorizontal: 72,
+        borderRadius: 40,
         elevation: 3,
         backgroundColor: 'white',
         borderWidth: 3,
         borderColor: '#8B19FF',
+        marginTop: 10,
     },
 
     login_button: {
@@ -122,7 +87,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         paddingVertical: 12,
         paddingHorizontal: 80,
-        borderRadius: 20,
+        borderRadius: 40,
         elevation: 3,
         backgroundColor: '#8B19FF',
         marginBottom: 10,
