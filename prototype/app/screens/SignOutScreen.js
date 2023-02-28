5669c1909d06938fa2bdb42454782e3203776850
Wedaad <wedaadh2@gmail.com>
2023-02-28 13:09:48 +0000

    change: UI of sign out screen

---
+++ prototype/app/screens/SignOutScreen.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import auth from '@react-native-firebase/auth';
+
+export default function SignOutScreen({navigation}) {
+
+    const handleSOPress = () => {
+        console.log("Sign-out button was just pressed");
+
+        auth()
+        .signOut()
+        .then(() => {
+            console.log('User signed out!');
+            navigation.navigate("Home", {screen: "Home"});
+            });
+    
+    };
+
+
+  return (
+    <View style={styles.screenLayout}>
+        <Text style={styles.title}>Logging Out...</Text>
+        <Text style={styles.subTitle}>Are you sure you want to sign out?</Text>
+
+        <View>
+            <Image source={require('../assets/icons/sign-out.png')} style={{width: 350, height: 350, resizeMode: 'contain', alignSelf: 'center', marginTop: 35}}/>
+        </View>
+
+        <View style={styles.buttonView}>
+            <TouchableOpacity style={styles.signOutButton} onPress={handleSOPress}>
+                <Text style={styles.buttonText}>Sign Out</Text>
+            </TouchableOpacity>
+        </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    screenLayout: {
+        // borderWidth: 4,
+        // borderColor: 'orange',
+        padding: 20,
+        flex: 1,
+        backgroundColor: 'white'
+    },
+
+    title: {
+        // borderWidth: 4,
+        // borderColor: 'red',
+        fontFamily: 'GTWalsheimPro-Regular',
+        marginTop: 10, 
+        fontSize: 25,
+        textAlign: "center",
+        
+    },
+
+    subTitle: {
+
+        margin: 10, 
+        fontSize: 17,
+        fontFamily: 'GTWalsheimPro-Regular',
+        textAlign: "center",
+
+    },
+
+    buttonView: {
+
+        marginTop: 70,
+        alignItems: 'center',
+    },
+
+    signOutButton: {
+
+        position: 'absolute',
+        padding: 10,
+        backgroundColor: '#8B19FF',
+        borderRadius: 10,
+    },
+
+    buttonText: {
+        fontFamily: 'GTWalsheimPro-Regular',
+        fontSize: 15,
+        color: "white",
+        justifyContent: 'flex-end',
+        padding: 5,
+      
+    },
+});
