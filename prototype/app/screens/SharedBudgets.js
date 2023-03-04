9105c3ffacfc755cd76143f1e703b1f16d70dd86
Wedaad <wedaadh2@gmail.com>
2023-03-04 11:49:00 +0000

    add: shared budget screen

--- prototype/app/screens/SharedBudgets.js
+++ prototype/app/screens/SharedBudgets.js
@@ -1,25 +1,81 @@
 import React from 'react';
-import { Text, View, StyleSheet} from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+
 
 export default function SharedBudgets() {
-  return (
 
-    <View  style={styles.screenLayout}>
-        <Text>Shared Budgets</Text>
-    </View>
-  )
+    const onAddGroupBudgetClick = () => {
+
+        console.log("Add group budget pressed")
+
+    }
+
+    return (
+
+        <View style={styles.screenLayout}>
+            <Text style={styles.title}>Shared Budgets</Text>
+
+            <View style={styles.addGroupButtonContainer}>
+                <TouchableOpacity style={styles.addGroupButton} onPress={onAddGroupBudgetClick}>
+                    <Text style={styles.buttonText}>Create Shared Budget</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
+  screenLayout: {
+      padding: 20,
+      flex: 1,
+      backgroundColor: 'white'
+  },
 
-    screenLayout: {
-        // borderWidth: 4,
-        // borderColor: 'orange',
-        flex: 1,
-        backgroundColor: 'white',
-        paddingTop: 30,
-        paddingHorizontal: 20,
-    },
+  title: {
+      fontFamily: 'GTWalsheimPro-Regular',
+      marginTop: 10, 
+      fontSize: 25,
+      textAlign: "center",
+      
+  },
 
+  subTitle: {
 
+      margin: 10, 
+      fontSize: 17,
+      fontFamily: 'GTWalsheimPro-Regular',
+      textAlign: "center",
+
+  },
+
+  buttonView: {
+
+      marginTop: 70,
+      alignItems: 'center',
+  },
+
+  addGroupButtonContainer: {
+
+    marginTop: 40,
+
+  },
+
+  addGroupButton: {
+
+      position: 'absolute',
+      padding: 10,
+      backgroundColor: '#8B19FF',
+      borderRadius: 10,
+      alignSelf: 'center'
+  },
+
+  buttonText: {
+      fontFamily: 'GTWalsheimPro-Regular',
+      fontSize: 15,
+      color: "white",
+      justifyContent: 'flex-end',
+      padding: 5,
+      // borderWidth: 1
+    
+  },
 });
