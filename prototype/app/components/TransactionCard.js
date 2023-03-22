cc202b2a7af3923de09453e8fcb6efad2d260165
Wedaad <wedaadh2@gmail.com>
2023-03-22 17:33:19 +0000

    fix: clean code

--- prototype/app/components/TransactionCard.js
+++ prototype/app/components/TransactionCard.js
@@ -2,19 +2,8 @@ import React, {useState} from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-export default function TransactionCard ({ amount, merchant, date}) {
+export default function TransactionCard ({amount, merchant, date}) {
 
-    // const [iconName, setIconName] = useState('');
-    
-    // // if the amount of the transaction is less than 0 (an expense)
-    // if (amount < 0) {
-
-    //     setIconName("bank-transfer-out")
-
-    // } else { // amount greater than 0 (income)
-
-    //     setIconName("bank-transfer-in")
-    // }
     const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
     const transacion_date = new Date(date)
     let day = date.slice(-2,)
@@ -27,16 +16,9 @@ export default function TransactionCard ({ amount, merchant, date}) {
        merchant = merchant.replace("From", "")
     }
 
-    // if(merchant.startsWith("To")) {
-
-    //     merchant = merchant.replace("To", "")
-    //  }
-
     // Flipping the signs of transactions
     if(amount < 0) { // if the amount retrieved is a negative (make it positive)
         amount = amount * -1
-
-        // return borderLeftColor: '#8B19FF'
 
     } else { // if the amount retrieved is a positive (make it negative)
 
@@ -49,12 +31,7 @@ export default function TransactionCard ({ amount, merchant, date}) {
         return (
 
             <View style={{borderLeftColor: "red", borderLeftWidth: 3,...styles.transactionCard}}>
-            {/* <View style={styles.iconContainer}> */}
-                {/* <MaterialCommunityIcons 
-                name={"bank-transfer-in"}
-                size={24} 
-                color="black"/> */}
-            {/* </View> */}
+
             <View style={styles.dateContainer}>
                 <View>
                     <Text style={{fontSize: 20, fontFamily: "GTWalsheimPro-Bold"}}>{day}</Text>
@@ -77,7 +54,7 @@ export default function TransactionCard ({ amount, merchant, date}) {
     } else {
 
         return (
-        
+            
             // <View style={{borderLeftColor: "#8B19FF", borderLeftWidth: 3,...styles.transactionCard}}>
             <View style={{borderLeftColor: "#5CD94E", borderLeftWidth: 3,...styles.transactionCard}}>
                 {/* <View style={styles.iconContainer}> */}
